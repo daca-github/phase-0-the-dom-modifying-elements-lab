49ca9f841760c93133c602f7758e041115bc67a6
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-04-05 18:35:44 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let main = document.querySelector('#main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Daniel is the champion";
